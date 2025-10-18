04818338340df833eabc055f9322ae6de319a593
athsara bimalka <152287082+athsarab@users.noreply.github.com>
2025-10-18 09:59:12 +0530

    Update server.js

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, { 
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
