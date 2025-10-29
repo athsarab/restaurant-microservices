2f314deada623bc15a35d64dbdbfd1fa27643255
athsara bimalka <152287082+athsarab@users.noreply.github.com>
2025-10-29 09:04:16 +0530

    Fix formatting issue in Order model reference

--- order-service/src/models/Order.js
+++ order-service/src/models/Order.js
@@ -11,7 +11,7 @@ const orderSchema = new mongoose.Schema({
       dish: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: 'Dish'
+        ref: 'Dish' 
       },
       name: String,
       price: Number, 
