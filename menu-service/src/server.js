ef3504c34c98712d31b9c3497cc375907ec86e55
athsara bimalka <152287082+athsarab@users.noreply.github.com>
2026-02-17 23:29:37 +0530

    Fix formatting in MongoDB connection log

--- menu-service/src/server.js
+++ menu-service/src/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB connected')) 
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
